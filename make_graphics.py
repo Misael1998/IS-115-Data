a5ac86227233ce0f643df2ceca1cd0c6d3754aa6
Misael1998 <misalande@gmail.com>
2021-08-13 18:05:34 -0600

    Frecuencia y conocimientos

--- make_graphics.py
+++ make_graphics.py
@@ -9,7 +9,7 @@ poblacion = estudiantes + trabajadores_de_bancos
 
 tamanio_de_poblacion = len(poblacion)
 
-# Calcular promedio habilidades blandas
+# Variables de habilidades blandas
 trabajo_en_equipo = []
 liderazgo = []
 manejo_de_estres = []
@@ -19,7 +19,13 @@ resolucion_de_problemas = []
 proactividad = []
 respeto_a_direcencias = []
 
+# Tecnologias y Frecuencia de actualizacion
+fre_actualizacion_tec_conocidas = []
+fre_aprendizaje_nueva_tec = []
+
+
 for individuo in poblacion:
+    #Habilidades blandas
     trabajo_en_equipo.append(individuo["Trabajo en equipo"])
     liderazgo.append(individuo["Liderazgo"])
     manejo_de_estres.append(individuo["Manejo del estrés\xa0"])
@@ -28,6 +34,13 @@ for individuo in poblacion:
     resolucion_de_problemas.append(individuo["Resolución de problemas"])
     proactividad.append(individuo["Proactividad"])
     respeto_a_direcencias.append(individuo["Respeto hacia las diferentes opiniones de los demás."])
+
+    #Tecnologias y Frecuencia de actualizacion
+    fre_actualizacion_tec_conocidas.append(individuo["De las tecnologías que usted domina ¿qué tan frecuente actualiza su conocimiento sobre estas?"])
+    fre_aprendizaje_nueva_tec.append(individuo["¿Qué tan frecuente aprende nuevas tecnologías?"])
+
+
+################ Habilidades Blandas ################
 
 trabajo_en_equipo = np.array(trabajo_en_equipo)
 liderazgo = np.array(liderazgo)
@@ -64,6 +77,78 @@ plt.savefig("./fig/habilidades_blandas.png", dpi=None, facecolor='w', edgecolor=
         metadata=None)
 plt.clf()
 
-#print("Trabajadores de banco: ",len(trabajadores_de_bancos))
-#print("Estudiantes: ",len(estudiantes))
+############## Tecnologias y Frecuencia ##############
+fre_act_set = list(set(fre_actualizacion_tec_conocidas))
+fre_apr_set = list(set(fre_aprendizaje_nueva_tec))
+
+frecuencia_act = [0] * len(fre_act_set)
+frecuencia_apr = [0] * len(fre_apr_set)
+
+for frecuencia in fre_actualizacion_tec_conocidas:
+    frecuencia_act[fre_act_set.index(frecuencia)] = frecuencia_act[fre_act_set.index(frecuencia)] + 1
+
+for frecuencia in fre_aprendizaje_nueva_tec:
+    frecuencia_apr[fre_apr_set.index(frecuencia)] = frecuencia_apr[fre_apr_set.index(frecuencia)] + 1
+
+frecuencia_act = np.array(frecuencia_act)
+frecuencia_apr = np.array(frecuencia_apr)
+
+frecuencia_act = (frecuencia_act / len(poblacion)) * 100
+frecuencia_apr = (frecuencia_apr / len(poblacion)) * 100
+
+fre_act_df = pd.DataFrame({
+    "Frecuencia": fre_act_set,
+    "Valor": frecuencia_act
+    })
+
+fre_apr_df = pd.DataFrame({
+    "Frecuencia": fre_apr_set,
+    "Valor": frecuencia_apr
+    })
+
+fre_act_df_ordenado = fre_act_df.sort_values("Valor", ascending=False)
+fre_apr_df_ordenado = fre_apr_df.sort_values("Valor", ascending=False)
+
+
+plt.bar('Frecuencia', 'Valor',data=fre_act_df_ordenado)
+plt.title("Frecuencia de actualización de conocimientos")
+plt.xlabel("Nivel de frecuencia")
+plt.ylabel("Frecuencia en %")
+plt.ylim([0,100])
+
+plt.savefig("./fig/actualizacion_de_conocimientos.png", dpi=None, facecolor='w', edgecolor='w',
+        orientation='portrait', format=None,
+        transparent=False, bbox_inches=None, pad_inches=0.1,
+        metadata=None)
+plt.clf()
+
+
+plt.bar('Frecuencia', 'Valor',data=fre_apr_df_ordenado)
+plt.title("Frecuencia de aprendizaje de conocimientos")
+plt.xlabel("Nivel de frecuencia")
+plt.ylabel("Frecuencia en %")
+plt.ylim([0,100])
+
+plt.savefig("./fig/aprendizaje_de_conocimientos.png", dpi=None, facecolor='w', edgecolor='w',
+        orientation='portrait', format=None,
+        transparent=False, bbox_inches=None, pad_inches=0.1,
+        metadata=None)
+plt.clf()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
